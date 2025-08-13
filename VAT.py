433f0b8d1040ede14df64a6f4e8c48ebc7ea508b
SantiCarvajalJulius <santiago.carvajal@julius2grow.com>
2025-08-13 09:55:12 -0500

    Hilos

--- VAT.py
+++ VAT.py
@@ -1,4 +1,12 @@
 import os
+
+# Limitar threads
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["OMP_NUM_THREADS"]      = "1"
+os.environ["MKL_NUM_THREADS"]      = "1"
+os.environ["NUMEXPR_NUM_THREADS"]  = "1"
+
+
 import pandas as pd
 import mysql.connector
 from dotenv import load_dotenv
